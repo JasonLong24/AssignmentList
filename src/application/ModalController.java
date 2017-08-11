ec544d46c2a9f9b0b9af3d78901ff202239a5ac7
JasonLong24 <jasonlongball@gmail.com>
2017-08-10 21:48:57 -0400

    added button and enter key

--- src/application/ModalController.java
+++ src/application/ModalController.java
@@ -1,5 +1,4 @@
 package application;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,7 +12,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
